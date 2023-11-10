33b41ef9772bfb9338de2d113b1ae78a1d96e015
shawn <xw2002121@gmail.com>
2023-11-10 23:31:21 +0800

    1

---
+++ hIndex.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int l=0;
+        int r=citations.size();
+        int cnt;
+        while(l<r){
+            cnt=0;
+            int mid=(l+r+1)>>1;
+            for(int i=0;i<citations.size();i++){
+                if(citations[i]>=mid){
+                    cnt++;
+                }
+            }
+            if(cnt>=mid){
+                l=mid;
+            }else{
+                r=mid-1;
+            }
+        }
+        return l;
+    }
+};
